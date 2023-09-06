91db7747ce2ed9fa008fa00e2835f779fafd03b4
movinsilva <movinpinsara@gmail.com>
2023-09-06 13:09:20 +0530

    db tables

--- src/database/db.sql
+++ src/database/db.sql
@@ -12,3 +12,90 @@ CREATE TABLE "User" (
     "PasswordHash" VARCHAR(255),
     "EmailConfirmed" BOOLEAN DEFAULT false
 );
+
+CREATE TABLE "Station" (
+    "StationID" SERIAL PRIMARY KEY,
+    "StationName" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "Wagon" (
+    "WagonID" SERIAL PRIMARY KEY,
+    "Capacity" INTEGER,
+    "Class" VARCHAR(255),
+    "SeatNoScheme" TEXT[][],
+    "TotalSeats" INTEGER,
+    "Description" VARCHAR(255),
+    "HasTables" BOOLEAN DEFAULT false
+);
+
+CREATE TABLE "TrainSchedule" (
+    "TrainNo" INTEGER PRIMARY KEY,
+    "TrainName" VARCHAR(255),
+    "Source" INTEGER REFERENCES "Station"("StationID"),
+    "Destination" INTEGER REFERENCES "Station"("StationID"),
+    "ArrivalTime" TIMESTAMP,
+    "DepartureTime" TIMESTAMP,
+    "TrainType" VARCHAR(255),
+    "Frequency" INTEGER REFERENCES "Frequency"("FrequencyID"),
+    "DefaultWagonsWithDirection" INTEGER[][],
+    "InvertedStations" INTEGER[],
+    "DefaultTotalSeats" INTEGER
+);
+
+CREATE TABLE "TrainStop" (
+    "stopID" INTEGER PRIMARY KEY,
+    "TrainNo" INTEGER REFERENCES "TrainSchedule"("TrainNo"),
+    "StationID" INTEGER REFERENCES "Station"("StationID"),
+    "ArrivalTime" TIMESTAMP,
+    "DepartureTime" TIMESTAMP,
+    "Load" INTEGER,
+    "PlatformNo" INTEGER
+);
+
+CREATE TABLE "Train" (
+    "TrainID" INTEGER PRIMARY KEY,
+    "TrainNo" INTEGER REFERENCES "TrainSchedule"("TrainNo"),
+    "Date" DATE,
+    "ChangedWagonsWithDirection" INTEGER[][]
+);
+
+CREATE TABLE "Frequency" (
+    "FrequencyID" INTEGER PRIMARY KEY,
+    "Name" VARCHAR(255),
+    "Monday" BOOLEAN,
+    "Tuesday" BOOLEAN,
+    "Wednesday" BOOLEAN,
+    "Thursday" BOOLEAN,
+    "Friday" BOOLEAN,
+    "Saturday" BOOLEAN,
+    "Sunday" BOOLEAN
+);
+
+CREATE TABLE "Booking" (
+    "BookingID" INTEGER PRIMARY KEY,
+    "userID" INTEGER REFERENCES "User"("UserID"),
+    "TrainID" INTEGER REFERENCES "TrainSchedule"("TrainNo"),
+    "isReturn" BOOLEAN,
+    "Source" INTEGER REFERENCES "Station"("StationID"),
+    "Destination" INTEGER REFERENCES "Station"("StationID"),
+    "BookedTime" TIMESTAMP,
+    "Amount" FLOAT,
+    "isPaid" BOOLEAN
+);
+
+CREATE TABLE "Passenger" (
+    "PassengerID" INTEGER REFERENCES "SeatBooking"("SeatBookingID"),
+    "NIC" VARCHAR(20),
+    "Name" VARCHAR(255),
+    "Gender" VARCHAR(10),
+    "Age" INTEGER,
+    PRIMARY KEY ("PassengerID")
+);
+
+CREATE TABLE "Payments" (
+    "PaymentID" INTEGER PRIMARY KEY,
+    "BookingID" INTEGER REFERENCES "Booking"("BookingID"),
+    "Timestamp" TIMESTAMP,
+    "isSuccess" BOOLEAN,
+    "Response" VARCHAR(255)
+);
